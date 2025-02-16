2379fab0f77432171c5c1c5f51bff6afc48765b1
Patrick Grasseels <grasseelsp@hotmail.com>
2025-02-17 00:20:13 +0100

    Init solution with dapr

--- src/Novalab.Master.Orchestration/Novalab.Master.Orchestration.AppHost/Program.cs
+++ src/Novalab.Master.Orchestration/Novalab.Master.Orchestration.AppHost/Program.cs
@@ -1,21 +1,38 @@
+using Aspire.Hosting.Dapr;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var mongo = builder.AddMongoDB("mongo")
+var mongo = builder.AddMongoDB("mongo", 27017)
                    .WithLifetime(ContainerLifetime.Persistent);
 
-var mongodb = mongo.AddDatabase("mongodb");
+var mongodb = mongo.AddDatabase("novalab", "novalab");
 
-var rabbitmq = builder.AddRabbitMQ("messaging")
-                      .WithManagementPlugin()
-                      .WithLifetime(ContainerLifetime.Persistent);
+var redis = builder.AddRedis("redis", 6380)
+                    .WaitFor(mongodb)
+                    .WithRedisInsight()
+                    .WithLifetime(ContainerLifetime.Persistent);
 
-var master = builder.AddProject<Projects.Novalab_Master_Host>("master")
-                    .WithReference(rabbitmq)
+var pubSub = builder.AddDaprPubSub("pubsub", new DaprComponentOptions()
+{
+    LocalPath = "./../../../components/"
+});
+
+var configuration = builder.AddDaprComponent("configuration", "configuration.redis", new DaprComponentOptions()
+{
+    LocalPath = "./../../../components/"
+});
+
+var master = builder.AddProject<Projects.Novalab_Master_Host>("masterservice")
+                    .WithReference(redis)
                     .WithReference(mongodb)
-                    .WaitFor(rabbitmq)
+                    .WithDaprSidecar("master")
+                    .WithReference(configuration)
+                    .WithReference(pubSub)
+                    .WaitFor(redis)
                     .WaitFor(mongodb);
 
-var frontend = builder.AddProject<Projects.Novalab_Master_Frontend>("frontend")
+var frontend = builder.AddProject<Projects.Novalab_Master_Frontend>("webfrontend")
                       .WithReference(master)
                       .WaitFor(master);
-builder.Build().Run();
+
+await builder.Build().RunAsync();
---
+++ src/Novalab.Master/Novalab.Master.Domain/Entities/Service.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Novalab.Master.Domain.Entities
+{
+    public class Service
+    {
+        [BsonId]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonRequired]
+        public string Name { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+
+        public bool IsEnabled { get; set; } = false;
+
+        [BsonRequired]
+        public string Uri { get; set; } = string.Empty;
+
+        [BsonRequired]
+        public string Version { get; set; } = string.Empty;
+
+        [BsonRequired]
+        public DateTime InstallAt { get; set; } = DateTime.UtcNow;
+
+        [BsonRequired]
+        public DateTime LoadAt { get; set; } = DateTime.UtcNow;
+
+        [BsonRequired]
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+    }
+}
